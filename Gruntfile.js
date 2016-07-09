b2438e93c9cc24364a71b37f37dc98851b69f4d3
gamgi <pietu_r@gmx.com>
2016-07-09 11:37:44 +0300

    added grunt-serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,6 +73,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-serve');
+
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
